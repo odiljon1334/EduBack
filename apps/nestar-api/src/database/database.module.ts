d3c01b8f7cb713425ead6dc6ea4f6bd6b1f7ebf6
odiljon1334 <odiljon9507@gmail.com>
2024-11-26 17:49:19 +0500

    feat: create components related moduls

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import {InjectConnection, MongooseModule} from '@nestjs/mongoose';
-import { log } from 'console';
 import {Connection} from 'mongoose';
 
 @Module({
