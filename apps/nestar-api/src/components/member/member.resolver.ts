1bf5f8e9018dce004b33f059d0a606a043a01650
odiljon1334 <odiljon9507@gmail.com>
2024-12-03 18:46:39 +0500

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -11,25 +11,14 @@ export class MemberResolver {
 
     @Mutation(() => Member)
     public async signup(@Args("input") input: MemberInput): Promise<Member> {
-        try {
             console.log('Mutation: signup');
             return this.memberService.signup(input);
-        } catch (err) {
-            console.log("Error, signup", err);
-            throw new InternalServerErrorException(err);
-        }
     }
 
     @Mutation(() => Member)
     public async login(@Args('input') input: LoginInput): Promise<Member> {
-        try {
             console.log('Mutation: login');
             return this.memberService.login(input);
-        } catch (err) {
-            console.log("Error, signup", err);
-            throw new InternalServerErrorException(err);
-        }
-        
     }
 
     @Mutation(() => String)
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -16,8 +16,8 @@ export class MemberService {
             // TODO: Authentication token
             return result;
         } catch (err) {
-            console.log("Error, Service.model:", err);
-            throw new BadRequestException(err);
+            console.log("Error, Service.model:", err.message);
+            throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
         }
     }
 
