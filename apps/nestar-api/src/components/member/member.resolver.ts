064105073a896ea6a0ab5f1e881178efb47ed439
odiljon1334 <odiljon9507@gmail.com>
2024-12-08 02:41:13 +0500

    feat: modify updateMember GraphQL API

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 
 @Resolver()
@@ -27,14 +28,6 @@ export class MemberResolver {
             return this.memberService.login(input);
     }
 
-    // Authenticated: (USER, AGENT, ADMIN)
-    @UseGuards(AuthGuard)
-    @Mutation(() => String)
-    public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
-        console.log('Mutation: updateMember');
-        return this.memberService.updateMember();
-    }
-
     @UseGuards(AuthGuard)
     @Query(() => String)
     public async checkAuth(@AuthMember('memberNick') memberNick: Member): Promise<string> {
@@ -46,11 +39,24 @@ export class MemberResolver {
     @Roles(MemberType.USER, MemberType.AGENT)
     @UseGuards(RolesGuard)
     @Query(() => String)
-    public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+    public async checkAuthRoles(
+        @AuthMember() authMember: Member): Promise<string> {
         console.log('Query: checkAuthRoles');
         return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
     }
 
+    // Authenticated: (USER, AGENT, ADMIN)
+    @UseGuards(AuthGuard)
+    @Mutation(() => Member)
+    public async updateMember(
+        @Args("input") input: MemberUpdate,
+        @AuthMember('_id') memberId: ObjectId): Promise<Member> {
+        console.log('Mutation: updateMember');
+        delete input._id;
+        return this.memberService.updateMember(memberId, input);
+    }
+
+    
     @Query(() => String )
     public async getMember(): Promise<string> {
         console.log('Query: getMember');
