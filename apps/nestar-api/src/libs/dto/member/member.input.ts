ddf26f1c2ad8e0203e28fb958c3c6460a2383c5f
odiljon1334 <odiljon9507@gmail.com>
2024-12-09 02:54:47 +0500

    feat: develop getAllMembersByAdmin and updateMembersByAdmin GrapQL API

--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType, Int } from "@nestjs/graphql";
 import { IsIn, IsNotEmpty, IsOptional, Length, Min } from "class-validator";
-import { MemberAuthType, MemberType } from "../../enums/member.enum";
-import { availableAgentSorts } from "../../config";
+import { MemberAuthType, MemberStatus, MemberType } from "../../enums/member.enum";
+import { availableAgentSorts, availableMembersSorts } from "../../config";
 import { Direction } from "../../enums/common.enum";
 
 
@@ -45,7 +45,7 @@ export class LoginInput {
 
 @InputType()
 export class AISearch {
-    @IsNotEmpty()
+    @IsOptional()
     @Field(() => String, {nullable: true})
     text?: string;
 }
@@ -74,5 +74,49 @@ export class AgentsInquiry {
     @IsNotEmpty()
     @Field(() => AISearch)
     search: AISearch;
+
+}
+
+
+@InputType()
+export class MISearch {
+
+    @IsOptional()
+    @Field(() => MemberStatus, {nullable: true})
+    memberStatus?: MemberStatus;
+
+    @IsOptional()
+    @Field(() => MemberType, {nullable: true})
+    memberType?: MemberType;
+
+    @IsOptional()
+    @Field(() => String, {nullable: true})
+    text?: string;
+}
+
+@InputType()
+export class MembersInquiry {
+    @IsNotEmpty()
+    @Min(1)
+    @Field(() => Int)
+    page: number;
+
+    @IsNotEmpty()
+    @Min(1)
+    @Field(() => Int)
+    limit: number;
+
+    @IsOptional()
+    @IsIn(availableMembersSorts)
+    @Field(() => String, {nullable: true})
+    sort?: string;
+
+    @IsOptional()
+    @Field(() => Direction, {nullable: true})
+    direction?: string;
+
+    @IsNotEmpty()
+    @Field(() => MISearch)
+    search: MISearch;
 
 }
