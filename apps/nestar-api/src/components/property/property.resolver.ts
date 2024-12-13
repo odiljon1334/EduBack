982f84405f8157aa012954a5dd114a91f5cfe913
odiljon1334 <odiljon9507@gmail.com>
2024-12-14 01:39:12 +0500

    feat: develop updatePropertyByAdmin GraphQL API

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -83,4 +83,16 @@ export class PropertyResolver {
         return await this.propertyService.getAllPropertiesByAdmin(input);
     
     }
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation((returns) => Property)
+    public async updatePropertyByAdmin(
+        @Args('input') input: PropertyUpdate, 
+        @AuthMember('_id') memberId: ObjectId
+    ): Promise<Property> {
+        console.log('Mutation: updatePropertyByAdmin');
+        input._id = shapeIntoMongoObjectId(input._id);
+        return await this.propertyService.updatePropertyByAdmin(input);
+    }
 }
