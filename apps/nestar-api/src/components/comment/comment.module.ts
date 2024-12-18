a7b2efb038dcc463e64c6f4b49393ba2349186a8
odiljon1334 <odiljon9507@gmail.com>
2024-12-18 13:55:50 +0500

    feat: integrated comment module related DTO

--- apps/nestar-api/src/components/comment/comment.module.ts
+++ apps/nestar-api/src/components/comment/comment.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { CommentResolver } from './comment.resolver';
+import { CommentService } from './comment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
+import { MemberModule } from '../member/member.module';
+import CommentSchema from '../../schemas/Comment.model';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{
+      name: "Comments",
+       schema: 
+       CommentSchema
+      }]),
+     AuthModule,
+     ViewModule,
+     MemberModule
+  ],
+  providers: [CommentResolver, CommentService]
+})
 export class CommentModule {}
---
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Comment } from '../../libs/dto/comment/comment';
+
+@Injectable()
+export class CommentService {
+    constructor(@InjectModel('Comments') private readonly commentService: Model<Comment>) {}
+}
