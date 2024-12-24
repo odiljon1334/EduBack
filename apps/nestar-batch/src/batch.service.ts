7e0d42140bbd9e8506c00e2272d4ed9068f25c12
odiljon1334 <odiljon9507@gmail.com>
2024-12-25 01:46:57 +0500

    feat: create batchRollBack, batchProperites, and batchAgnts, jobSchedulers logic

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -5,4 +5,16 @@ export class BatchService {
   getHello(): string {
     return 'Welcome to Nestar-Batch Server!';
   }
+
+  public async batchRollBack(): Promise<void> {
+    console.log("batchRollBack");
+  }
+
+  public async batchProperties(): Promise<void> {
+    console.log("batchProperties");
+  }
+
+  public async batchAgents(): Promise<void> {
+    console.log("batchAgents");
+  }
 }
