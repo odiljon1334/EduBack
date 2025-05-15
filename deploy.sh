9e413804d85f8697cc1e6d00f16e6935b9e861ef
odiljon1334 <odiljon9507@gmail.com>
2025-05-16 00:15:19 +0500

    fix: modify  deploy shell for production

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,3 @@ git pull origin master
 
 docker compose down
 docker compose up -d
-
-# DEVELOPMENT
-# git reset --hard
-# git checkout develop
-# git pull origin develop
-
-# npm i
-# pm2 start "npm run start:dev" --name=AsianFoods
